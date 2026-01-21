0e9287f74e5fecb486a320bcc20c4c48feeb953a
Knovek <pqnarte@gmail.com>
2026-01-21 08:35:18 +0100

    added icons for tiered clues

--- src/main/java/com/oneshot/utils/Icons.java
+++ src/main/java/com/oneshot/utils/Icons.java
@@ -16,6 +16,13 @@ public class Icons {
     public static final BufferedImage LEVEL_IMAGE = ImageUtil.loadImageResource(Icons.class, "/iconStats.png");
     public static final BufferedImage TASKS_IMAGE = ImageUtil.loadImageResource(Icons.class, "/iconTasks.png");
 
+    public static final BufferedImage CLUE_SCROLL_BEGINNER = ImageUtil.loadImageResource(Icons.class, "/Clue_scroll_(beginner)_detail.png");
+    public static final BufferedImage CLUE_SCROLL_EASY = ImageUtil.loadImageResource(Icons.class, "/Clue_scroll_(easy)_detail.png");
+    public static final BufferedImage CLUE_SCROLL_MEDIUM = ImageUtil.loadImageResource(Icons.class, "/Clue_scroll_(medium)_detail.png");
+    public static final BufferedImage CLUE_SCROLL_HARD = ImageUtil.loadImageResource(Icons.class, "/Clue_scroll_(hard)_detail.png");
+    public static final BufferedImage CLUE_SCROLL_ELITE = ImageUtil.loadImageResource(Icons.class, "/Clue_scroll_(elite)_detail.png");
+    public static final BufferedImage CLUE_SCROLL_MASTER = ImageUtil.loadImageResource(Icons.class, "/Clue_scroll_(master)_detail.png");
+
     public static final ImageIcon RED_HELM = new ImageIcon(RED_HELM_IMAGE);
     public static final ImageIcon RED_HELM_SMALLER = new ImageIcon(RED_HELM_IMAGE.getScaledInstance(Constants.BUTTON_SIZE -2, Constants.BUTTON_SIZE -2, Image.SCALE_DEFAULT));
     public static final ImageIcon WORLD = new ImageIcon(WORLD_IMAGE.getScaledInstance(12, 12, Image.SCALE_SMOOTH));
