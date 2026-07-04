70bf602219fb611b8cae0a17191612b7ceaad986
Knovek <pqnarte@gmail.com>
2026-07-04 19:29:45 +0200

    updated version

--- src/main/java/com/oneshot/OneShotConfig.java
+++ src/main/java/com/oneshot/OneShotConfig.java
@@ -17,7 +17,7 @@ public interface OneShotConfig extends Config
             name = "version",
             description = "version",
             hidden = true)
-    default String version() {return "v1.3.1"; }
+    default String version() {return "v1.3.2"; }
 
     // --- Sections ---
     @ConfigSection(
